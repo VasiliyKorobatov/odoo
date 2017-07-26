72c1fee36bd6a86d98b92f58caf4f245311d14c2
Василий Коробатов <vasiliy@korobatov.ru>
2017-07-26 12:40:41 +0500

    custom theme

--- addons/theme_l/__manifest__.py
+++ addons/theme_l/__manifest__.py
@@ -1,16 +1,18 @@
 # -*- encoding: utf-8 -*-
 # Not Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+
 {
     'name': 'L theme',
     'description': 'L theme',
     'version': '0.1',
     'author': 'Vasiliy Korobatov',
     'data': [
-        'views/layout.xml',
         'views/assets.xml',
-        'views/snippets.xml',
-        'views/pages.xml'
+        'views/layout.xml',
+        'views/options.xml',
+        'views/pages.xml',
+        'views/snippets.xml'
     ],
     'category': 'Theme/Creative',
     'depends': ['website', 'sale'],
