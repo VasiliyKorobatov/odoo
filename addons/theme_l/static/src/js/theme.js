d7249b88e5e05233a785da3bc9fdf95c449e713d
Василий Коробатов <vasiliy@korobatov.ru>
2017-07-29 00:53:22 +0500

    custom theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -5,14 +5,14 @@ var navHeight = $('.navbar').outerHeight(true) + 10;
 
 $('#sidebar').affix({
       offset: {
-        top: 400,
-        bottom: navHeight
+        top: 60,
+        bottom: 600
       }
 });
 
 
 $body.scrollspy({
 	target: '#leftCol',
-	offset: navHeight
+	offset: 600
 });
 });
