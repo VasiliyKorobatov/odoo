9d7520bfd55d04c33e7d360faf0feb0c1ecb1737
Василий Коробатов <vasiliy@korobatov.ru>
2017-07-29 21:02:41 +0500

    custom theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -5,7 +5,9 @@ var navHeight = $('.navbar').outerHeight(true) + 10;
 
 $('#sidebar').affix({
       offset: {
-        top: 150,
+        top: function(){
+            return ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)++$("header").outerHeight(true)
+        },
         bottom: function(){
             return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))
 //            return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$("#wrapwrap").outerHeight(true))
@@ -16,6 +18,8 @@ $('#sidebar').affix({
 
 $body.scrollspy({
 	target: '#leftCol',
-	offset: 250
+	offset: function(){
+            return ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)++$("header").outerHeight(true)
+        }
 });
 });
