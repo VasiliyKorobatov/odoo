3f940d799c25dad6ba24ae5c674c176076347b63
Василий Коробатов <vasiliy@korobatov.ru>
2017-07-29 14:57:48 +0500

    custom theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -7,7 +7,7 @@ $('#sidebar').affix({
       offset: {
         top: 150,
         bottom: function(){
-            return ($("footer").outerHeight(true)-$("#wrapwrap").outerHeight(true))
+            return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$("#wrapwrap").outerHeight(true))
         }
       }
 });
