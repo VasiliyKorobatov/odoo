a1f0c4cb0f00928de72a26fbb963b16d6981e9fb
Василий Коробатов <vasiliy@korobatov.ru>
2017-07-29 21:07:51 +0500

    custom theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 
-            var $body   = $(document.body);
-var navHeight = $('.navbar').outerHeight(true) + 10;
+var $body   = $(document.body);
+var navHeight = function(){
+      if ($("#oe_main_menu_navbar").length){
+        return ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)
+      } else
+        return ($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)
+      }
 
+}
 $('#sidebar').affix({
       offset: {
-        top: function(){
-            return ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)++$("header").outerHeight(true)
-        },
+        top: navHeight,
         bottom: function(){
             return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))
 //            return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$("#wrapwrap").outerHeight(true))
@@ -18,8 +22,6 @@ $('#sidebar').affix({
 
 $body.scrollspy({
 	target: '#leftCol',
-	offset: function(){
-            return ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)++$("header").outerHeight(true)
-        }
+	offset: navHeight
 });
 });
