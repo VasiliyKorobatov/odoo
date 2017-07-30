5898b2116c9b947f6b5a4dc12f736003e973468f
Василий Коробатов <vasiliy@korobatov.ru>
2017-07-30 13:57:44 +0500

    custom theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
-if($("div#wrap").length && $("#sidebar").outerHeight(true)>$("div#wrap").outerHeight(true)){
-    $("#sidebar div.oe_structure").hide();
-}
-var $body   = $(document.body);
-var navHeight = $("#oe_main_menu_navbar").length ? ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)) :($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true));
 
-$('#sidebar').affix({
-      offset: {
-        top: navHeight,
-        bottom: function(){
-            return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))
-//            return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$("#wrapwrap").outerHeight(true))
-        }
-      }
+    var $body   = $(document.body);
+    var navHeight = $("#oe_main_menu_navbar").length ? ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)) :($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true));
+
+    $('#sidebar').affix({
+          offset: {
+            top: navHeight,
+            bottom: function(){
+                return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))
+    //            return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$("#wrapwrap").outerHeight(true))
+            }
+          }
+    });
+
+
+    $body.scrollspy({
+        target: '#leftCol',
+        offset: navHeight
+    });
 });
-
-
-$body.scrollspy({
-	target: '#leftCol',
-	offset: navHeight
-});
+$(window).load(function() {
+    if($("div#wrap").length && $("#sidebar").outerHeight(true)>$("div#wrap").outerHeight(true)){
+        $("#sidebar div.oe_structure").hide();
+    }
 });
