aaa0d9db51a8f97b9bb55f0ff562d33d045acf1d
Василий Коробатов <vasiliy@korobatov.ru>
2017-07-30 23:54:14 +0500

    custom theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -1,34 +1,32 @@
 $(document).ready(function() {
-
+//    var $body   = $(document.body);
+//    var navHeight = function(){ return($("#oe_main_menu_navbar").length ? ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)) :($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true))) };
+//    $('#sidebar').affix({
+//          offset: {
+//            top: navHeight,
+//            bottom: function(){return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))}
+//          }
+//    });
+//    $body.scrollspy({
+//        target: '#leftCol',
+//        offset: navHeight
+//    });
+});
+$(window).load(function() {
     var $body   = $(document.body);
-    var navHeight = $("#oe_main_menu_navbar").length ? ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)) :($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true));
-
+    var navHeight = function(){ return($("#oe_main_menu_navbar").length ? ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)) :($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true))) };
     $('#sidebar').affix({
           offset: {
             top: navHeight,
             bottom: function(){return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))}
           }
     });
-//    $('div.dropdown_sorty_by').affix({
-//          offset: {
-//            top: navHeight
-////            ,
-////            bottom: function(){return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))}
-//          }
-//    });
-
-
-    $body.scrollspy({
-        target: '#leftCol',
-        offset: navHeight
-    });
-//    $body.scrollspy({
-//        target: 'div.dropdown_sorty_by',
-//        offset: navHeight
-//    });
-});
-$(window).load(function() {
     if($("div#wrap").length && $("#sidebar").outerHeight(true)>$("div#wrap").outerHeight(true)){
-        $("#sidebar div.oe_structure").hide();
+//        $("#sidebar div.oe_structure").hide();
+    } else {
+        $body.scrollspy({
+            target: '#leftCol',
+            offset: navHeight
+        });
     }
 });
