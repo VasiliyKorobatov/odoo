1fb4517ff7c4f5a64378b5a8099a330f9e49b052
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-01 18:19:38 +0500

    custom theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
-//    var $body   = $(document.body);
-//    var navHeight = function(){ return($("#oe_main_menu_navbar").length ? ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)) :($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true))) };
-//    $('#sidebar').affix({
-//          offset: {
-//            top: navHeight,
-//            bottom: function(){return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))}
-//          }
-//    });
-//    $body.scrollspy({
-//        target: '#leftCol',
-//        offset: navHeight
-//    });
+    $('#sidebar').on('show.bs.collapse', function(){
+        $("main").attr("class","col-md-9");
+    });
+    $('#sidebar').on('hidden.bs.collapse', function(){
+        $("main").attr("class","col-md-12");
+    });
 });
 $(window).load(function() {
 
