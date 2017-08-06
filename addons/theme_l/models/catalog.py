2ac3c5d5a0d230147f16319cac730e609fc3efcb
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-06 21:03:35 +0500

    custom theme

--- addons/theme_l/models/catalog.py
+++ addons/theme_l/models/catalog.py
@@ -5,9 +5,11 @@ from odoo import api, fields, models
 class ProductCatalogType(models.Model):
     _name = 'product.catalog.type'
     name = fields.Char(string="Name", required=True)
+    product_template = fields.Many2one('product.template', string="Product Template", ondelete='set null', index=True )
 
 class ProductCatalogModel(models.Model):
     _name = 'product.catalog.model'
     name = fields.Char(string="Name", required=True)
+    product_product = fields.Many2many('product.product', string="Product Variant", ondelete='set null', index=True  )
 
 
