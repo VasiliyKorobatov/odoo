bbaeedc285939e5a4bb982cd2ab49e77c6b8f993
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-07 11:44:40 +0500

    custom theme

--- addons/theme_l/models/catalog.py
+++ addons/theme_l/models/catalog.py
@@ -6,7 +6,7 @@ class ProductCatalogType(models.Model):
     _name = 'product.catalog.type'
     name = fields.Char(string="Name", required=True)
     product_template = fields.Many2many('product.template', string="Product Template", ondelete='set null', index=True )
-    product_templates = fields.One2many('product.template', inverse_name="catalog_type", string="Product Template",  index=True )
+    product_templates = fields.One2many('product.template', inverse_name="catalog_type", string="Product Template",  auto_join=True)
 
 class ProductCatalogModel(models.Model):
     _name = 'product.catalog.model'
