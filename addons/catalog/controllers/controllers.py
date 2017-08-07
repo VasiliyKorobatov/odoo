1eb58d8aeec3ee7037f27953483bcd8b30216145
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-07 15:08:44 +0500

    custom theme

---
+++ addons/catalog/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+# class Catalog(http.Controller):
+#     @http.route('/catalog/catalog/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/catalog/catalog/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('catalog.listing', {
+#             'root': '/catalog/catalog',
+#             'objects': http.request.env['catalog.catalog'].search([]),
+#         })
+
+#     @http.route('/catalog/catalog/objects/<model("catalog.catalog"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('catalog.object', {
+#             'object': obj
+#         })
