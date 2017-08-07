caccf6368d0849905229a8c16686f498aa2be4a1
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-07 14:40:36 +0500

    custom theme

--- addons/theme_l/models/product.py
+++ addons/theme_l/models/product.py
@@ -5,7 +5,6 @@ from odoo import api, fields, models
 
 
 class Product(models.Model):
-    _inherit = 'product.template'
     dimensions_uom_id = fields.Many2one(
         'product.uom',
         'Unit of Measure',
