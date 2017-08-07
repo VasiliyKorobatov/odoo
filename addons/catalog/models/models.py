d04603fe535babe37fd8dfb5aa95bf02fca1c404
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-07 16:22:04 +0500

    custom theme

--- addons/catalog/models/models.py
+++ addons/catalog/models/models.py
@@ -9,8 +9,12 @@ class ProductCatalogType(models.Model):
 class ProductCatalogModel(models.Model):
     _name = 'catalog.model'
     name = fields.Char(string="Name", required=True)
+    display_name = fields.Char(compute='_compute_display_name', store=True, index=True)
     catalog_type = fields.Many2one(comodel_name="catalog.type", string="Catalog Type", required=True, )
     product_product = fields.Many2many('product.product', 'mod_prod_rel', 'model_id', 'product_id', string="Product Variant", ondelete='set null', index=True  )
+
+    def _compute_display_name(self):
+        self.display_name = self.catalog_type.name+" "+self.name
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
