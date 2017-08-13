cd7516f9bfba6dfc43a71df891a9d01c30e5050e
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-13 14:48:20 +0500

    theme

--- addons/website_multi_image_zoom/static/src/js/website_sale.js
+++ addons/website_multi_image_zoom/static/src/js/website_sale.js
@@ -2,25 +2,25 @@ odoo.define('website_multi_image_zoom.website_sale', function(require) {
 
     var ajax = require('web.ajax');
     $(document).ready(function() {
-//        $('.oe_website_sale').each(function() {
-//            var demo = '<div class="xzoom-container"><img class="xzoom" id="xzoom-default" xoriginal="' + $('#ex1').find('img').attr("src") + '" style="width: 300px; height: 300px;" src="' + $('#ex1').find('img').attr("src") + '"/></div>';
-//            $('#ex1 span').html(demo);
-//            var oe_website_sale = this;
-//
-//            function price_to_str(prices) {
-//            	var price = prices;
-//                price = Math.round(price * 100) / 100;
-//                var dec = Math.round((price % 1) * 100);
-//                return price +
-//                    (dec
-//                        ? ''
-//                        : '.0') +
-//                    (dec % 10
-//                        ? ''
-//                        : '0');
-//            }
-//
-//            $(oe_website_sale).on('change', 'input.js_variant_change, select.js_variant_change, ul[data-attribute_value_ids]', function(ev) {
+        $('.oe_website_sale').each(function() {
+            var demo = '<div class="xzoom-container"><img class="xzoom" id="xzoom-default" xoriginal="' + $('#ex1').find('img').attr("src") + '" style="width: 300px; height: 300px;" src="' + $('#ex1').find('img').attr("src") + '"/></div>';
+            $('#ex1 span').html(demo);
+            var oe_website_sale = this;
+
+            function price_to_str(prices) {
+            	var price = prices;
+                price = Math.round(price * 100) / 100;
+                var dec = Math.round((price % 1) * 100);
+                return price +
+                    (dec
+                        ? ''
+                        : '.0') +
+                    (dec % 10
+                        ? ''
+                        : '0');
+            }
+
+            $(oe_website_sale).on('change', 'input.js_variant_change, select.js_variant_change, ul[data-attribute_value_ids]', function(ev) {
 //                var $ul = $(ev.target).closest('ul.js_add_cart_variants');
 //                var $parent = $ul.closest('.js_product');
 //                var product_id = $parent.find('input.product_id').first().val();
@@ -43,12 +43,12 @@ odoo.define('website_multi_image_zoom.website_sale', function(require) {
 //                } else {
 //                    $img.attr("src", "/website/image/product.template/" + $('#product_img_display').val() + "/image");
 //                }
-//            });
-//
-//            $('ul.js_add_cart_variants', oe_website_sale).each(function() {
-//                $('input.js_variant_change, select.js_variant_change', this).first().trigger('change');
-//            });
-//
-//        });
+            });
+
+            $('ul.js_add_cart_variants', oe_website_sale).each(function() {
+                $('input.js_variant_change, select.js_variant_change', this).first().trigger('change');
+            });
+
+        });
     });
 });
