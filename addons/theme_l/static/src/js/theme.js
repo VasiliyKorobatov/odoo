054b78d6ab94eb1a297cbed0cde3efd3d7bd3a4b
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-15 13:28:14 +0500

    theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -19,26 +19,6 @@ $(document).ready(function() {
         update_variant();
     });
 });
-$(window).load(function() {
-
-//
-//    if($("div#wrap").length && $("#sidebar").outerHeight(true)>$("div#wrap").outerHeight(true)){
-////        $("#sidebar div.oe_structure").hide();
-//    } else {
-//        var $body   = $(document.body);
-//        var navHeight = function(){ return($("#oe_main_menu_navbar").length ? ($("#oe_main_menu_navbar").outerHeight(true)+$(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true)) :($(".navbar-fixed-top").outerHeight(true)+$("header").outerHeight(true))) };
-//        $('#sidebar').affix({
-//              offset: {
-//                top: navHeight,
-//                bottom: function(){return ($("footer").outerHeight(true)+$("div#sidebar").outerHeight(true)-$(document).outerHeight(true))}
-//              }
-//        });
-//        $body.scrollspy({
-//            target: '#leftCol',
-//            offset: navHeight
-//        });
-//    }
-});
 
 function update_variant(){
 	Array.prototype.diff = function(a) {
@@ -46,23 +26,25 @@ function update_variant(){
 	};
 	var attrs = [];
 	var price;
-	$(".js_variant_change").each(function(i,e){attrs.push(parseInt($(e).val()))});
-	prc = $.parseJSON($("ul.js_add_cart_variants").attr("data-attribute_value_ids"));
-	prc.forEach(function (item,i,arr) { if (attrs.diff(item[1]).length == 0){
-	    variant_id = arr[i][0];
-	    price = arr[i][3];
-	}});
-	if(typeof price != "undefined")
-	    $(".oe_currency_value").text((price.toFixed(2)).replace('.',','));
-	odoo.define('website.product', function(require) {'use strict'; var Model = require('web.Model');
-        var Product = new Model('product.product');
-        Product.call('read', [[variant_id],['weight','volume','length','width','height','default_code']]).then(function(result){
-        $("#weight-value").text(result[0]['weight']);
-        $("#volume-value").text(result[0]['volume']);
-        $("#lhw-value").text(result[0]['length']+'x'+result[0]['width']+'x'+result[0]['height']);
-        $('#default_code').text(result[0]['default_code']);
-    });
-    $("#xzoom-default").attr("src",'/website/image/product.product/'+variant_id+'/image');
-    $("#xzoom-default").attr("xoriginal",'/website/image/product.product/'+variant_id+'/image');
-});
+	if($(".js_variant_change").length){
+        $(".js_variant_change").each(function(i,e){attrs.push(parseInt($(e).val()))});
+        prc = $.parseJSON($("ul.js_add_cart_variants").attr("data-attribute_value_ids"));
+        prc.forEach(function (item,i,arr) { if (attrs.diff(item[1]).length == 0){
+            variant_id = arr[i][0];
+            price = arr[i][3];
+        }});
+        if(typeof price != "undefined")
+            $(".oe_currency_value").text((price.toFixed(2)).replace('.',','));
+        odoo.define('website.product', function(require) {'use strict'; var Model = require('web.Model');
+            var Product = new Model('product.product');
+            Product.call('read', [[variant_id],['weight','volume','length','width','height','default_code']]).then(function(result){
+                $("#weight-value").text(result[0]['weight']);
+                $("#volume-value").text(result[0]['volume']);
+                $("#lhw-value").text(result[0]['length']+'x'+result[0]['width']+'x'+result[0]['height']);
+                $('#default_code').text(result[0]['default_code']);
+            });
+        });
+        $("#xzoom-default").attr("src",'/website/image/product.product/'+variant_id+'/image');
+        $("#xzoom-default").attr("xoriginal",'/website/image/product.product/'+variant_id+'/image');
+    }
 }
