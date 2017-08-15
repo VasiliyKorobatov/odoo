822a9259aee1bc34ba230968cdf1dfb6d1e7acb5
Василий Коробатов <vasiliy@korobatov.ru>
2017-08-15 13:14:20 +0500

    theme

--- addons/theme_l/static/src/js/theme.js
+++ addons/theme_l/static/src/js/theme.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         $("#city").modal('hide');
     });
     update_variant();
-    $("#product-card .js_variant_change").change(function(){
+    $("#product-card .js_add_cart_variants .js_variant_change").change(function(){
         update_variant();
     });
 });
