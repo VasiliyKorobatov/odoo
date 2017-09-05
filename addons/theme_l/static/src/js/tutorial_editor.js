6d2fd550fc0b06d12d4138fbe677d7e8fc54df70
Василий Коробатов <vasiliy@korobatov.ru>
2017-09-05 13:58:35 +0500

    custom theme

--- addons/theme_l/static/src/js/tutorial_editor.js
+++ addons/theme_l/static/src/js/tutorial_editor.js
@@ -1,9 +1,9 @@
-//odoo.define('snippet_tmonial_options',function(require){
-//    'use strict';
-//    var options = require("web_editor.snippets.options");
-//    options.registry.snippet_tmonial_opt = options.Class.extend({
-//        on_focus: function(){
-//            alert("On focus!");
-//        },
-//    });
-//});
+odoo.define('snippet_tmonial_options',function(require){
+    'use strict';
+    var options = require("web_editor.snippets.options");
+    options.registry.snippet_tmonial_opt = options.Class.extend({
+        on_focus: function(){
+            alert("On focus!");
+        },
+    });
+});
