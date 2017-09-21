b6b82dccbe2e16ae397e7f9c39e244b76b12735c
Василий Коробатов <vasiliy@korobatov.ru>
2017-09-21 19:52:25 +0500

    some

---
+++ addons/web_list_html_widget/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'HTML Widget in tree',
+    'category': 'Web',
+    'author': 'Somebody',
+    'summary': '',
+    'website': '',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'description': '',
+    'depends': [
+    ],
+    'data': [
+        "views/views.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+}
--- addons/web_list_html_widget/static/src/js/view_list.js
+++ addons/web_list_html_widget/static/src/js/view_list.js
@@ -1,10 +1,36 @@
-openerp.web_list_html_widget = function(instance){
-    instance.web.list.columns.map['field.html'] = 'instance.web.list.HtmlColumn';
+//openerp.web_list_html_widget = function(instance){
+//    instance.web.list.columns.map['field.html'] = 'instance.web.list.HtmlColumn';
+//
+//    instance.web.list.HtmlColumn = instance.web.list.Column.extend({
+//        _format: function (row_data, options) {
+//            return instance.web.format_value(
+//                row_data[this.id].value, this, options.value_if_empty);
+//        }
+//    });
+//};
 
-    instance.web.list.HtmlColumn = instance.web.list.Column.extend({
-        _format: function (row_data, options) {
-            return instance.web.format_value(
-                row_data[this.id].value, this, options.value_if_empty);
-        }
+    odoo.define('html_in_tree_field.web_ext', function (require) {
+
+        "use strict";
+
+     var Listview = require('web.ListView');
+
+     var formats = require('web.formats');
+
+
+
+     Listview.Column.include({
+
+         _format: function (row_data, options) {
+
+      // Removed _.escape() function to display html content.
+
+      // Before : return _.escape(formats.format_value(row_data[this.id].value, this, options.value_if_empty));
+
+      return formats.format_value(row_data[this.id].value, this, options.value_if_empty);
+
+         }
+
+     });
+
     });
-};
