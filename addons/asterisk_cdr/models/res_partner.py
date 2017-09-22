840d4835b73767e12f3777d5c2d632d173ac84ff
Василий Коробатов <vasiliy@korobatov.ru>
2017-09-22 16:39:36 +0500

    some some

--- addons/asterisk_cdr/models/res_partner.py
+++ addons/asterisk_cdr/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
 
 import logging
@@ -10,6 +10,8 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     call_count = fields.Integer(compute='_compute_calls', string='# of Calls')
+
+    @api.one
     def _compute_calls(self):
         call_data = self.env['asterisk.cdr'].search([('to_partner_id', '=', self.id)])
         _logger.info("Calls %s" % call_data)
