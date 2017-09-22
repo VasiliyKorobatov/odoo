a33569cb0c197614b8d214090a7e7fdff76350ad
Василий Коробатов <vasiliy@korobatov.ru>
2017-09-22 16:30:21 +0500

    some some

--- addons/asterisk_cdr/models/res_partner.py
+++ addons/asterisk_cdr/models/res_partner.py
@@ -4,10 +4,13 @@
 from odoo import fields, models
 from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     call_count = fields.Integer(compute='_compute_sale_order_count', string='# of Calls')
     def _compute_sale_order_count(self):
-        call_data = self.env['asterisk.cdr'].search(['|',('to_partner_id', '=', self.id),('from_partner_id', '=', self.id)])
+        call_data = self.env['asterisk.cdr'].search([('to_partner_id', '=', self.id)])
+        _logger.info(call_data)
         self.call_count = len(call_data)
