9ffb6261ffd3a88cd23a77c2c930b37192e1a652
Василий Коробатов <vasiliy@korobatov.ru>
2017-10-02 12:45:02 +0500

    [IMP] add fix style addon

---
+++ addons/web_responsive_plus/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Web Responsive Plus",
+    "summary": "Web Responsive Plus is upgrade Web Responsive Module"
+               "web",
+    "version": "10.0.0.0.1",
+    "category": "Website",
+    "website": "https://korobatov.ru/",
+    "author": "Vasiliy Korobatov",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'web_responsive',
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/web.xml',
+    ],
+    # 'qweb': [
+    #     'static/src/xml/form_view.xml',
+    # ],
+}
