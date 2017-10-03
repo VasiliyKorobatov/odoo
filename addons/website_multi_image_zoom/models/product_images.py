8fcc94c71c64a1bd40c300dbe8988ec89c74fb86
Василий Коробатов <vasiliy@korobatov.ru>
2017-10-03 18:16:31 +0500

    [IMP] Compute Small image

--- addons/website_multi_image_zoom/models/product_images.py
+++ addons/website_multi_image_zoom/models/product_images.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models, tools
 
 
 class ProductImage(models.Model):
@@ -12,12 +12,34 @@ class ProductImage(models.Model):
     description = fields.Text(string='Description')
     image_alt = fields.Text(string='Image Label')
     image = fields.Binary(string='Image')
-    image_small = fields.Binary(string='Small Image')
+    image_small = fields.Binary(string='Small Image', compute='_compute_images')
+    # image_small = fields.Binary(string='Small Image', compute='_compute_images', inverse='_set_image_small')
     image_url = fields.Char(string='Image URL')
     product_tmpl_id = fields.Many2one('product.template', 'Product',
                                       copy=False)
     product_variant_id = fields.Many2one('product.product', 'Product Variant',
                                          copy=False)
+
+    @api.one
+    @api.depends('image')
+    def _compute_images(self):
+        if self._context.get('bin_size'):
+            self.image_small = self.image
+        else:
+            resized_images = tools.image_get_resized_images(self.image, return_big=True, avoid_resize_medium=True)
+            self.image_small = resized_images['image_small']
+
+    @api.one
+    def _set_image_small(self):
+        self._set_image_value(self.image_small)
+    #
+    # @api.one
+    # def _set_image_value(self, value):
+    #     image = tools.image_resize_image_big(value)
+    #     if self.product_tmpl_id.image:
+    #         self.image_variant = image
+    #     else:
+    #         self.product_tmpl_id.image = image
 
 
 class ProductProduct(models.Model):
