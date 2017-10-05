d23a127d32223545b02f3c24590a6b775713fd55
Василий Коробатов <vasiliy@korobatov.ru>
2017-10-05 18:15:32 +0500

    [DEV] Dev theme

--- addons/theme_l/models/website.py
+++ addons/theme_l/models/website.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class Website(models.Model):
     _inherit = "website"
+
+    content_before = fields.Html("Content In Top")
+    content_after = fields.Html("Content In Bottom")
 
     @api.model
     def sale_categories(self):
