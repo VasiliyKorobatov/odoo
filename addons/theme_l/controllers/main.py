b8435843936a112fc2bff931a9ec7b5911931431
Василий Коробатов <vasiliy@korobatov.ru>
2017-11-20 17:36:04 +0500

    [fix] z-index static top menu

--- addons/theme_l/controllers/main.py
+++ addons/theme_l/controllers/main.py
@@ -5,9 +5,10 @@ from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-class WebsiteSale(WebsiteSale):
+class WebsiteSaleOptions(WebsiteSale):
 
-    @http.route('/website/data/product',  type='json', auth="public", website=True)
-    def product(self,  **kwargs):
-        p = request.env['product.product'].sudo().search([('id', '=', kwargs.get('product_id'))])
-        return {'website_price':p.website_price,'weight':p.weight,'volume':p.volume,'length':p.length,'width':p.width,'height':p.height,'default_code':p.default_code}
+    @http.route(['/website/data/product'],  type='json',  methods=['POST'], auth="public", website=True)
+    def product(self, product_id, **kwargs):
+        p = request.env['product.product'].sudo().search([('id', '=', product_id)])
+        # return {'website_price':p.website_price,'weight':p.weight,'volume':p.volume,'length':p.length,'width':p.width,'height':p.height,'default_code':p.default_code}
+        return True
