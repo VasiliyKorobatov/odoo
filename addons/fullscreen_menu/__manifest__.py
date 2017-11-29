03b4fb925c24580b31f1cfc896251f270e2cbad7
Василий Коробатов <vasiliy@korobatov.ru>
2017-11-29 12:54:29 +0500

    [imp] fullscreen menu

---
+++ addons/fullscreen_menu/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Fullscreen Menu",
+
+    'summary': """
+        Replace Menu To Fullscreen menu""",
+
+    'description': """
+        Replace Menu To Fullscreen menu
+    """,
+
+    'author': "Vasiliy Korobatov",
+    'website': "http://korobatov.ru",
+
+    'category': 'Website',
+    'version': '10.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+}
