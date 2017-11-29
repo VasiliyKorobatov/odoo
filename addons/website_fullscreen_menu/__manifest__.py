8ec6e0fe8c8fc0cf688c2417ba6313c73d1c8cdf
Василий Коробатов <vasiliy@korobatov.ru>
2017-11-29 15:47:17 +0500

    [rev]  add assets

--- addons/website_fullscreen_menu/__manifest__.py
+++ addons/website_fullscreen_menu/__manifest__.py
@@ -22,6 +22,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         # 'views/views.xml',
+        'views/assets.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
